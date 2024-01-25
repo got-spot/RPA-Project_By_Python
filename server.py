8d85654f4525c31c4d02c080d77f0411d4ed516f
got-spot <137681097+got-spot@users.noreply.github.com>
2024-01-25 11:15:56 +0900

    form to db connect

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import pymysql
 from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
-
+from fetch_samearea import fetch_samearea
 db = SQLAlchemy()
 
 app = Flask(__name__)
@@ -43,14 +43,17 @@ class corpTable(db.Model):
     est_dt = db.Column(db.Text)
     acc_mt = db.Column(db.Text)
 
-@app.route('/')
-def index():
+@app.route('/') # 홈페이지
+def index(): # home
     return form()
 
-@app.route('/corp/<name>')
-def corp_res(name):
+@app.route('/corp/companies', methods = ['POST', 'GET'])
+def corp_res():
+    if request.method == 'POST':
+        corp = request.form["corp"]
+        #return render_template('index.html', result=result)
     try:
-        a = db.session.query(corpTable.induty_code).filter(corpTable.corp_name.like(f'%{name}%')) # 종목코드 
+        a = db.session.query(corpTable.induty_code).filter(corpTable.corp_name.like(f'%{corp}%')) # 종목코드 
         query = db.session.query(corpTable.bizr_no,corpTable.stock_name, corpTable.ceo_nm).filter(corpTable.induty_code == a).order_by(corpTable.stock_name)
         corps = query.all()
 
@@ -65,14 +68,44 @@ def corp_res(name):
         hed = '<h1>Something is broken.</h1>'
         return hed + error_text
 
+@app.route('/corp/fetch_samearea', methods = ['POST', 'GET'])
+def corp_fetch():
+    if request.method == 'POST':
+        corp = request.form["corp"]
+        #return render_template('index.html', result=result)
+    try:
+        ret = fetch_samearea(corp)
+        if ret :
+            msg = '성공'
+        else:   
+            msg = '오류'
+        return msg
+    
+    except Exception as e:
+        # e holds description of the error
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
+
 def form():
     content = '''
-       <form action="/corp/">
+       <form action="/corp/companies" method = 'POST'>
             <input type="text" name="corp" placeholder="기업 이름 입력">
             <input type="submit" value="검색">
         </form>
     '''
     return content
+
+@app.route('/fetch')
+def fetch_form():
+    content = '''
+       <form action="/corp/fetch_samearea" method = 'POST'>
+            <input type="text" name="corp" placeholder="기업 이름 입력">
+            <input type="submit" value="검색">
+        </form>
+    '''
+    return content
+
 
 if __name__ == '__main__':
     app.run(debug=True)
