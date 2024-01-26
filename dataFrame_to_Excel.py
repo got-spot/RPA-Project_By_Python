3289f3b932357c28d29777671091ee8afdc09b42
got-spot <137681097+got-spot@users.noreply.github.com>
2024-01-26 16:53:30 +0900

    pandas 2.0 update 이후 append 문법 삭제
    -> concat method 사용

--- dataFrame_to_Excel.py
+++ dataFrame_to_Excel.py
@@ -3,31 +3,24 @@ import glob
 import pandas as pd
 
 # specifying the path to csv files
-# path = "C:/downloads"
-path='C:\\Users\\rlaal\\OneDrive\\바탕 화면\\Python_Rpa\\'
+path=r'C:\Users\rlaal\OneDrive\바탕 화면\Python_Rpa\\'
 
 # csv files in the path
 file_list = glob.glob(path + "/*.xlsx")
 
 # list of excel files we want to merge.
-# pd.read_excel(file_path) reads the excel
-# data into pandas dataframe.
+# pd.read_excel(file_path) reads the 
+# excel data into pandas dataframe.
 excl_list = []
 
 for file in file_list:
 	excl_list.append(pd.read_excel(file))
 
-# create a new dataframe to store the 
-# merged excel file.
-excl_merged = pd.DataFrame()
+# concatenate all DataFrames in the list
+# into a single DataFrame, returns new 
+# DataFrame.
+excl_merged = pd.concat(excl_list, ignore_index=True)
 
-for excl_file in excl_list:
-	
-	# appends the data into the excl_merged 
-	# dataframe.
-	excl_merged = excl_merged.append(
-	excl_file, ignore_index=True)
-
-# exports the dataframe into excel file with
-# specified name.
-excl_merged.to_excel('total_food_sales.xlsx', index=False)
+# exports the dataframe into excel file
+# with specified name.
+excl_merged.to_excel('Bank_Stocks.xlsx', index=False)
