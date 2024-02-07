ce461b96354be5b44ec8e4b0fd2dbd88cdd9010a
got-spot <137681097+got-spot@users.noreply.github.com>
2024-02-07 16:01:21 +0900

    02/07 작업 파일

--- Function.py
+++ Function.py
@@ -1,161 +1,25 @@
-import os
-path=r'C:\Users\rlaal\OneDrive\바탕 화면\Python_Rpa\\'
-def XlsxDelete():
-    file_list = os.listdir(path) 
-    for file in file_list: # 엑셀 파일들 삭제
-        name, ext = os.path.splitext(file)
-        if ext == '.xlsx':
-            file_path = os.path.join(path, file)
-            os.remove(file_path)
 
-import os, time, csv, re, json, sys, pandas, base64
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC 
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
-import mysql.connector
-from selenium.common import exceptions
-from selenium.common.exceptions import WebDriverException
-from openpyxl import Workbook,load_workbook
-
-def CretopExcel(corp):
-    wb = Workbook() # 새 워크북 생성
-    ws = wb.active # 현재 활성화된 sheet 가져옴
-
-
-    def clearPerformanceLog():
-        browser.get_log('performance')
-
+# from flask import request
+import mysql.connector,datetime
+def submitCrawlJob(corp_list):
     mydb = mysql.connector.connect(
     host="localhost",
     user="root",
     password="admin",
     database="dart"
     )
-
-    # mycursor = mydb.cursor()
-    # mycursor.execute(same_area_query_sql)
-    # companies = mycursor.fetchall()
-
-    caps = DesiredCapabilities.CHROME.copy() 
-    caps['goog:loggingPrefs'] = {"performance": "ALL"}
-    options = ChromeOptions()
-    options.add_experimental_option('debuggerAddress', '127.0.0.1:9222')
-    browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options, desired_capabilities=caps)
-
-    # -------------------------------------------------------
-    url = "https://new.cretop.com"
-    browser.get(url) # 크레탑으로 이동
-    wait = WebDriverWait(browser, 10)
-    time.sleep(3)
+    now = datetime.datetime.now()
+    query = f"insert into dart.crawl_job values (null, 0, now()); SELECT LAST_INSERT_ID();"
+    mycursor = mydb.cursor()
+    mycursor.execute(query)
+    data = mycursor.fetchall()
+    #mycursor.close()
+    print(data)
+    inserted_id = data
+    for bizr_no in corp_list:
+        query = f"insert into dart.job_item values (null, {bizr_no}, {inserted_id})"
+        mycursor.execute(query)
+    return data[0]
 
 
-    for company in len(corp): 
-        # print(company[2])  #corp_name
-        browser.get("https://new.cretop.com/ET/SS/ETSS070M1") # 크레탑(기업)으로 이동
-        time.sleep(4)
-
-        elem = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#app > div.wrap > header > div > div.top > div.search-form__area.sub > div.search-form > input[type=text]:nth-child(1)")))
-        elem.send_keys(company) # 사업자 등록번호 입력
-        time.sleep(4)
-
-        elem = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "i[class='search icon50 icon50 search']")))
-        elem.click() # 검색 아이콘 클릭
-        time.sleep(4)
-
-        elem = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#app > div.wrap > div.container > div > div > div:nth-child(2) > div:nth-child(3) > div.search-result__area > div > ul > li > div > ul.btn__list > li:nth-child(4) > a"))) # 재무 탭
-        browser.execute_script("arguments[0].click()", elem) #  검색결과 페이지 로딩완료 및 재무단추 클릭
-        # a[title='재무 페이지로 이동하기']
-        time.sleep(4)
-
-        elem = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "label[for='account-standard-2']")))
-        elem.click()     # 일반기업회계 단추 클릭
-        time.sleep(3)
-
-        elem = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#forms > option:nth-child(2)")))
-        elem.click() # 전계정
-        time.sleep(3)
-
-        elem = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#etfi110m1 > div > div:nth-child(3) > div > div > div > div.tab-item.teb-item1 > div > div.inner__area.search-top__area > div > div.btn-wrap > button")))
-        elem.click() # 조회하기
-        time.sleep(4)
-
-        file = next((file for file in os.listdir('.') if re.match('^ETFI', file)), None)
-        if file:
-            os.remove(file) # 추가한 파일 삭제
-        clearPerformanceLog()
-        time.sleep(3)
-
-        elem = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[class='btn excel-download']")))
-        # button[class='btn excel-download']
-        # browser.execute_script("arguments[0].click()", elem)
-        elem.click() # 엑셀(재무상태표) 다운
-        time.sleep(3) # 다운로드 대기
-
-        filename = next((file for file in os.listdir('.') if re.match('^ETFI', file)), None)
-
-        #파일 갯수 만큼 -> 한 업체당 엑셀 표 3개 반복
-        wb_src = load_workbook(filename)
-
-        # fp = open('ETFI112E1.xlsx','rb').read() # 재무상태표 insert
-        # fp = base64.b64encode(fp)
-        # args = (fp, company[0])
-        # mycursor.execute(excel_insertion_sql,args)
-        # mydb.commit()
-
-        elem = wait.until(EC.presence_of_element_located((By.LINK_TEXT, "손익계산서")))
-        elem.click()
-        time.sleep(3)
-
-        file = next((file for file in os.listdir('.') if re.match('^ETFI', file)), None)
-        if file:
-            os.remove(file) # 추가한 파일 삭제
-        clearPerformanceLog()
-
-        elem = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#etfi110m1 > div > div:nth-child(3) > div > div > div > div.tab-item.teb-item1 > div > div:nth-child(3) > div > div > div > div.tab-item.teb-item2 > div > div > div.table__area > div > div.content__head > div > div > button")))
-        elem.click() # 손익계산 다운
-        time.sleep(3) # 다운로드 대기
-
-        # fp = open('ETFI112E1.xlsx','rb').read() # 손익계산서 insert
-        # fp = base64.b64encode(fp)
-        # args = (fp, company[0])
-        # mycursor.execute(excel_2_sql,args) 
-        # mydb.commit()
-
-        elem = wait.until(EC.presence_of_element_located((By.LINK_TEXT, "재무분석")))
-        elem.click()
-        time.sleep(4)
-
-        elem = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "label[for='account-standard-4']")))
-        elem.click() # 일반기업회계
-        time.sleep(3)
-
-        elem = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "button[class='btn default w-240 h-56']")))
-        browser.execute_script("arguments[0].click()", elem) # 조회하기(재무분석)
-        time.sleep(3)
-
-        file = next((file for file in os.listdir('.') if re.match('^ETFI', file)), None)
-        if file:
-            os.remove(file) # 추가한 파일 삭제
-        clearPerformanceLog()
-
-        elem = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#etfi110m1 > div > div:nth-child(3) > div > div > div > div.tab-item.teb-item4 > div > div:nth-child(3) > div.table__area > div.content__head > div > div > button"))) # 재무분석 
-        browser.execute_script("arguments[0].click()", elem) # 다운로드 클릭
-        time.sleep(3) # 다운로드 대기
-
-    # try:
-    #     fp = open('ETFI114E1.xlsx','rb').read() # 재무분석 insert
-    #     fp = base64.b64encode(fp)
-    #     args = (fp, company[0])
-    #     mycursor.execute(excel_3_sql,args) 
-    #     mydb.commit()
-    # except:
-    #     pass
-    # return wb.save("동종업종.xlsx") 
+  
--- server.py
+++ server.py
@@ -2,9 +2,12 @@ import pymysql
 from flask import Flask, request, render_template, Response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
-from getExcelStreamByCompanyName import getExcelStreamByCompanyName
-db = SQLAlchemy()
+import mysql.connector
+# from getExcelStreamByCompanyName import getExcelStreamByCompanyName
+from Function import submitCrawlJob
+from make_excel import getExcelStreamByCompanyName
 
+db = SQLAlchemy()
 app = Flask(__name__)
 
 # MySQL 연결 설정
@@ -56,74 +59,46 @@ def form():
     '''
     return content
 
+
 @app.route('/corp/companies', methods = ['POST', 'GET'])
 def corp_res():
     if request.method == 'POST':
+        global corp 
         corp = request.form["corp"]
-        #return render_template('index.html', result=result)
     try:
         a = db.session.query(corpTable.induty_code).filter(corpTable.corp_name.like(f'%{corp}%')) # 종목코드 
         query = db.session.query(corpTable.bizr_no,corpTable.stock_name, corpTable.ceo_nm).filter(corpTable.induty_code == a) # .order_by(corpTable.stock_name)
         corps = query.all()
-
-        corp_text = '<ul>'
+        # corp_text = '<form action="/corp/companies/CretopDown" method = "POST"><ul>'
+        corp_text = '<form action="/crawlRequest" method = "POST"><ul>'
         for corp in corps:
-            corp_text += f'<li> {corp.bizr_no}  :  {corp.stock_name} </li>'
-        corp_text += '</ul>'
-        return corp_text + '<input type="button" value="동종업종 엑셀 다운">'
+            corp_text += f'''
+            <li> 
+                <input type="checkbox" name="check" value="{corp.bizr_no}"> {corp.bizr_no}  :  {corp.stock_name} 
+                
+            </li>
+            '''
+        corp_text += '</ul><input type="submit" value="엑셀파일 생성 시작"> </form>'
+        return corp_text
     except Exception as e:
         # e holds description of the error
         error_text = "<p>The error:<br>" + str(e) + "</p>"
         hed = '<h1>Something is broken.</h1>'
-        return hed + error_text
-    
-# @app.route('/corp/companies', methods = ['POST', 'GET'])
-def GetExcel():
-    content = '''
-       <form action="/corp/companies/xlsx" method = 'POST'>
-            <input type="submit" name="xlsx" value="동종업종 엑셀파일 생성">
-        </form>
-    '''
-    return content
-    
-"""
-@app.route('/corp/fetch_samearea', methods = ['POST', 'GET'])
-def corp_fetch():
-    if request.method == 'POST':
-        corp = request.form["corp"]
-        #return render_template('index.html', result=result)
-    try:
-        ret = fetch_samearea(corp)
-        if ret :
-            msg = '성공'
-        else:   
-            msg = '오류'
-        return msg
-    
-    except Exception as e:
-        # e holds description of the error
-        error_text = "<p>The error:<br>" + str(e) + "</p>"
-        hed = '<h1>Something is broken.</h1>'
-        return hed + error_text
+        return hed + error_text 
 
-@app.route('/fetch')
-def fetch_form():
-    content = '''
-       <form action="/corp/fetch_samearea" method = 'POST'>
-            <input type="text" name="corp" placeholder="기업 이름 입력">
-            <input type="submit" value="검색">
-        </form>
-    '''
-    return content
-"""
-
-@app.route('/download', methods = ['POST', 'GET']) # URL 설정하기
-def download():
-    # CSV 파일 형태로 브라우저가 파일다운로드라고 인식하도록 만들어주기
-    if request.method == 'POST':
-        corp = request.form["corp"]
-        #return render_template('index.html', result=result)
-    output = getExcelStreamByCompanyName(corp)
+@app.route('/corp/companies/CretopDown', methods = ['POST', 'GET']) # URL 설정하기
+def CretopDown():
+    corp_list = request.form.getlist('check')
+    corp_text = '('
+    i = 0
+    for bizr_no in corp_list:
+        corp_text += "'" + bizr_no + "'"
+        i += 1
+        if i < len(corp_list):
+            corp_text += ","
+    corp_text += ')'
+    output = getExcelStreamByCompanyName(corp_text)
+    # return output
     response = Response(
         output.getvalue(),
         mimetype="application/vnd.ms-excel",
@@ -132,6 +107,36 @@ def download():
     response.headers["Content-Disposition"] = "attachment; filename=merged.xlsx" # 다운받았을때의 파일 이름 지정해주기
     return response
 
+
+@app.route('/crawlRequest', methods = ['POST', 'GET']) # URL 설정하기
+def crawlRequest():
+    corp_list = request.form.getlist('check')
+    output = submitCrawlJob(corp_list)
+    # return output
+
+    return output
+
+
+
+
+
+
+
+
+
+# @app.route('/download', methods = ['POST', 'GET']) # URL 설정하기
+# def download():
+#     # CSV 파일 형태로 브라우저가 파일다운로드라고 인식하도록 만들어주기
+#     if request.method == 'POST':
+#         corp = request.form["corp"]
+#     output = getExcelStreamByCompanyName('대한항공')
+#     response = Response(
+#         output.getvalue(),
+#         mimetype="application/vnd.ms-excel",
+#         content_type='application/octet-stream',
+#     )
+#     response.headers["Content-Disposition"] = "attachment; filename=merged.xlsx" # 다운받았을때의 파일 이름 지정해주기
+#     return response
 
 if __name__ == '__main__':
     app.run(debug=True)
